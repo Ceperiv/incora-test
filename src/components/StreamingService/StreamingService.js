4eacafa730313594bab497dd8a57a7fd3c3f40ca
Andriy Konoval <ceperiv@dmail.com>
2022-08-24 12:15:52 +0300

    cat-kitty video

--- src/components/StreamingService/StreamingService.js
+++ src/components/StreamingService/StreamingService.js
@@ -15,9 +15,8 @@ function StreamingService() {
         <div className={'StreamingService'}>
             {showsInfo.map(showInfo => <Show key={showInfo.id} showInfo={showInfo} episode={episode} series={series}/>)}
             <h3><i>keep calm:)</i></h3>
-            <iframe src="https://embed.api.video/vod/vi3SBl1di71A89BWy85tEZLx" width="300px" height="100%"
-                    frameBorder="0" scrolling="no" allowFullScreen="true">keep clam
-            </iframe>
+            <iframe src="https://embed.api.video/vod/view7g3enH3bIggnvy9MjS5" width="300px" height="100%" frameBorder="0"
+                    scrolling="no" allowFullScreen="true">keep calm</iframe>
         </div>
     );
 }
